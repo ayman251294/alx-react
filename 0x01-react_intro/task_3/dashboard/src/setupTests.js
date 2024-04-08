b790ccd2efad6186dd229760299b969e1108cdee
ayman251294 <ayman251294@gmail.com>
2024-04-08 15:45:16 +0000

    new update

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,21 +1,4 @@
-import { getFooterCopy, getFullYear, getLatestNotification } from './utils';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-describe("Utils functions", () => {
-  
-  test("getFullYear returns the correct year", () => {
-    expect(getFullYear()).toEqual(2021);
-  });
-
-  test("getFooterCopy returns the correct string when the argument is true", () => {
-    expect(getFooterCopy(true)).toEqual("Holberton School");
-  });
-
-  test("getFooterCopy returns the correct string when the argument is false", () => {
-    expect(getFooterCopy(false)).toEqual("Holberton School main dashboard");
-  });
-
-  test("getLatestNotification returns the expected string", () => {
-    expect(getLatestNotification()).toEqual("<strong>Urgent requirement</strong> - complete by EOD");
-  });
-
-});
+configure({ adapter: new Adapter() });
